4d2cec11117eccabe4754fe02c97cd619f898550
Guilherme Rambo <insidegui@gmail.com>
2017-05-27 22:37:58 -0300

    Removed main thread precondition

--- Source/Siesta/Support/GCD+Siesta.swift
+++ Source/Siesta/Support/GCD+Siesta.swift
@@ -12,10 +12,5 @@ extension DispatchQueue
     {
     internal static func mainThreadPrecondition(caller: String = #function)
         {
-        precondition(
-            Thread.isMainThread,
-            "Illegal attempt to use Siesta method \"\(caller)\" from a background thread. " +
-            "Except in specific situations, you must call Siesta APIs from the main thread. " +
-            "See https://bustoutsolutions.github.io/siesta/guide/threading/")
-        }
+			// DISABLED FOR XPC
     }
